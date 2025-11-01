1c7a242d83e9a1f8bea67a415b18c4f564ab73ac
shoaibquraishi06 <dxshoaib51@gmail.com>
2025-11-01 10:42:07 +0530

    s

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ debug: true });
+require('dotenv').config();
 const app = require('./src/app')
 const connectDB = require('./src/db/db')
 const initSocketServer = require('./src/sockets/socket.server')
